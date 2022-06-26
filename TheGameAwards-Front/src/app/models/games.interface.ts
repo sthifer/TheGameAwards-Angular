9bd83e1849ec1d41dc404033a98eb3906cc2fc8c
Jay <javier.heartagram@gmail.com>
2022-06-26 21:52:16 +0200

    Game show

---
+++ TheGameAwards-Front/src/app/models/games.interface.ts
@@ -0,0 +1,13 @@
+export interface GamesInterface{
+    title:"string",
+    developer:"string",
+    date:"string"
+    cover:string,
+    description:"string",
+    genre:"string",
+    platform:"string",
+    votes: number,
+    deletedid: number,
+    id: number,
+    
+}
