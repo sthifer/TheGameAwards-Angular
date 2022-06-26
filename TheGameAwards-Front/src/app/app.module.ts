6fd42e5102b28a8d03319822a7950bb2cad245a8
alvaroFGG <alvarofuentenebrogomez@gmail.com>
2022-06-26 16:48:48 +0200

    header and footer created

--- TheGameAwards-Front/src/app/app.module.ts
+++ TheGameAwards-Front/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
