48f7f80dafce0c5ff5e97c2ec4ef73014880843d
alvaroFGG <alvarofuentenebrogomez@gmail.com>
2022-06-26 16:56:07 +0200

    Squashed commit of the following:
    commit e3bb8bdaa22d053e86370f291858b8f8c8539d2c
    Merge: 27be0ba 793bc0a
    Author: sthifer <sthifer21@gmail.com>
    Date:   Sun Jun 26 13:03:54 2022 +0200
    
        Merge branch 'javier_dev'
    
    commit 27be0ba85787cdb88cc7bef73433fd2623d759fb
    Author: sthifer <sthifer21@gmail.com>
    Date:   Sun Jun 26 13:03:45 2022 +0200
    
        Create .gitignore
    
    commit 793bc0a407c6f15edb1f1962868550d7cc3c1f0e
    Author: sthifer <sthifer21@gmail.com>
    Date:   Sat Jun 25 17:11:03 2022 +0200
    
        create
    
        create
    
    commit af1536d6e1b09acc27368d3d49b49d1def41c403
    Author: sthifer <sthifer21@gmail.com>
    Date:   Sat Jun 25 15:47:22 2022 +0200
    
        db.json relleno
    
        Primeros 3 juegos de la lista
    
    commit 4c2d24ed044224e4faef64b4b6981057657d1c3b
    Merge: 55c6cb2 23dd6bf
    Author: sthifer <sthifer21@gmail.com>
    Date:   Sat Jun 25 14:15:21 2022 +0200
    
        Merge branch 'main' into javier_dev
    
    commit 55c6cb2589cf98ee9d1c905809ac28aee61c939d
    Author: sthifer <sthifer21@gmail.com>
    Date:   Sat Jun 25 14:14:59 2022 +0200
    
        ignore
    
        ignore

--- TheGameAwards-Front/src/app/app.module.ts
+++ TheGameAwards-Front/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './core/header/header.component';
-import { FooterComponent } from './core/footer/footer.component';
+import { GamesManagementComponent } from './pages/games-management/games-management.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent
+    GamesManagementComponent
   ],
   imports: [
     BrowserModule,
