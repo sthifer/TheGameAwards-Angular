e59feb7decdc95e76ebbc39037bdf83a9ee5f35f
sthifer <sthifer21@gmail.com>
2022-06-26 20:30:17 +0200

    asa
    sa

--- TheGameAwards-Front/src/app/app.module.ts
+++ TheGameAwards-Front/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GamesManagementComponent } from './pages/games-management/games-management.component';
 
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,9 +15,11 @@ import { GamesManagementComponent } from './pages/games-management/games-managem
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SweetAlert2Module
   ],
   providers: [],
   bootstrap: [AppComponent]
+  
 })
 export class AppModule { }
--- TheGameAwards-Front/src/app/pages/games-management/games-management.component.ts
+++ TheGameAwards-Front/src/app/pages/games-management/games-management.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 @Component({
   selector: 'app-games-management',
   templateUrl: './games-management.component.html',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GamesManagementComponent implements OnInit {
 
-  constructor() { }
+  public gamesForm!: FormGroup;
+
+  constructor(/*private formBuilder: FormBuilder, private router: Router*/) { }
 
   ngOnInit(): void {
+  }
+
+  public onSubmit() {
+    
   }
 
 }
