6bdbe3ac6f7bb3b8877acc49efc87f3502764356
alvaroFGG <alvarofuentenebrogomez@gmail.com>
2022-06-26 19:11:32 +0200

    request games service created
    se ha creado el servicio, tambien se ha importado en los providers, y se ha importado en el componente de management, asi que solo quedaria hacer las funciones correspondientes

---
+++ TheGameAwards-Front/src/app/services/request-games.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RequestGamesService {
+
+  constructor(private httpClient:HttpClient) { }
+
+  public gameData = {
+    title: "",
+    description: "",
+    genre: "",
+    votes: "",
+    platform: [],
+    img: "",
+    deletedid: "",
+    id: ""
+  }
+
+  public clearGame(){
+    this.gameData = {
+      title: "",
+      description: "",
+      genre: "",
+      votes: "",
+      platform: [],
+      img: "",
+      deletedid: "",
+      id: ""
+    }
+  }
+
+  public editItem(item:any){
+    this.gameData = item;
+  }
+
+  public getGames(){
+    return this.httpClient.get("http://localhost:3000/games");
+  }
+
+  public postGame(newGame:any){
+    return this.httpClient.post("http://localhost:3000/games", newGame);
+  }
+
+  public deleteGame(gameId:any){
+    return this.httpClient.delete("http://localhost:3000/games/"+gameId);
+  }
+
+  public editGame(gameId:any, editedGame:any){
+    this.httpClient.put("http://localhost:3000/games/"+gameId, editedGame);
+  }
+
+}
